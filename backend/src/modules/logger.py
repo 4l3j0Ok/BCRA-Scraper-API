0329919025f76eac82b6e153ccc8d6a75bac70e5
AlejoSarmiento <alejofsarmiento@gmail.com>
2023-08-27 23:44:37 -0300

    logger module

---
+++ backend/src/modules/logger.py
@@ -0,0 +1,13 @@
+import logging
+from modules import config
+
+
+logging.basicConfig(
+    filename = config.LOG_PATH,
+    encoding = 'utf-8',
+    level = logging.DEBUG,
+    format = "%(asctime)s MODULE: %(module)s (%(funcName)s) - LINE: %(lineno)d - %(levelname)s - %(message)s",
+    datefmt = '%d/%m/%Y %H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
